35e0e71244e903c440525d92a1a06f1ac957e036
AlvaroHG1 <19109865@student.hhs.nl>
2023-06-28 17:25:49 +0200

    Code-smell: Long methode, verwijderd

--- src/main/java/com/example/verhuurbedrijf/Vensters/Toevoegen.java
+++ src/main/java/com/example/verhuurbedrijf/Vensters/Toevoegen.java
@@ -1,11 +1,9 @@
 package com.example.verhuurbedrijf.Vensters;
 
+import com.example.verhuurbedrijf.Observable;
 import com.example.verhuurbedrijf.ProductFactory;
 import com.example.verhuurbedrijf.ProductInventaris;
 import com.example.verhuurbedrijf.Venster;
-import com.example.verhuurbedrijf.factorys.BoorMachineFactory;
-import com.example.verhuurbedrijf.factorys.PersonenAutoFactory;
-import com.example.verhuurbedrijf.factorys.VrachtAutoFactory;
 import com.example.verhuurbedrijf.soorten.BoorMachine;
 import com.example.verhuurbedrijf.soorten.PersonenAuto;
 import com.example.verhuurbedrijf.soorten.VrachtAuto;
@@ -28,54 +26,52 @@ public class Toevoegen extends Venster {
         System.out.println("Product toevoegen:");
         voegProductToe();
     }
-
     void voegProductToe(){
-
         if (productSoort.equals("boormachine")) {
-            System.out.println("Boormachine");
-            System.out.print("Omschrijving: ");
-            String omschrijving = scanner.nextLine();
-            System.out.print("Merk: ");
-            String merk = scanner.nextLine();
-            System.out.print("Type: ");
-            String type = scanner.nextLine();
-            BoorMachine nieuweBoormachine = ProductFactory.Boor.createProduct();
-            nieuweBoormachine.setOmschrijving(omschrijving);
-            nieuweBoormachine.setMerk(merk);
-            nieuweBoormachine.setType(type);
-            productInventaris.addProduct(nieuweBoormachine);
+            voegBoormachineToe();
         } else if (productSoort.equals("personenauto")) {
-            System.out.println("Personenauto");
-            System.out.print("Omschrijving: ");
-            String omschrijving = scanner.nextLine();
-            System.out.print("Merk: ");
-            String merk = scanner.nextLine();
-            System.out.print("Gewicht in kg: ");
-            int gewicht = scanner.nextInt();
-            System.out.print("Motorinhoud in cc: ");
-            int motorInhoud = scanner.nextInt();
-            PersonenAuto nieuwePersonenAuto = ProductFactory.PersonenAuto.createProduct();
-            nieuwePersonenAuto.setOmschrijving(omschrijving);
-            nieuwePersonenAuto.setMerk(merk);
-            nieuwePersonenAuto.setGewicht(gewicht);
-            nieuwePersonenAuto.setMotorInhoud(motorInhoud);
+            voegPersonenautoToe();
         } else {
-            System.out.println("Vrachtwagen");
-            System.out.print("Omschrijving: ");
-            String omschrijving = scanner.nextLine();
-            System.out.print("Laadvermogen in kg: ");
-            int laadvermogen = scanner.nextInt();
-            System.out.print("Motorinhoud in cc: ");
-            int motorInhoud = scanner.nextInt();
-            VrachtAuto nieuweVrachtAuto = ProductFactory.Vrachtauto.createProduct();
-            nieuweVrachtAuto.setOmschrijving(omschrijving);
-            nieuweVrachtAuto.setLaadVermogen(laadvermogen);
-            nieuweVrachtAuto.setMotorInhoud(motorInhoud);
+            voegVrachtautoToe();
         }
         System.out.println("Succesvol toegevoegd");
         sluitVenster();
     }
-
+    void voegBoormachineToe(){
+        System.out.println("Boormachine");
+        BoorMachine nieuweBoormachine = ProductFactory.Boor.createProduct();
+        System.out.print("Omschrijving: ");
+        nieuweBoormachine.setOmschrijving(scanner.nextLine());
+        System.out.print("Merk: ");
+        nieuweBoormachine.setMerk(scanner.nextLine());
+        System.out.print("Type: ");
+        nieuweBoormachine.setType(scanner.nextLine());
+        productInventaris.addProduct(nieuweBoormachine);
+    }
+    void voegPersonenautoToe(){
+        System.out.println("Personenauto");
+        PersonenAuto nieuwePersonenAuto = ProductFactory.PersonenAuto.createProduct();
+        System.out.print("Omschrijving: ");
+        nieuwePersonenAuto.setOmschrijving(scanner.nextLine());
+        System.out.print("Merk: ");
+        nieuwePersonenAuto.setMerk(scanner.nextLine());
+        System.out.print("Gewicht in kg: ");
+        nieuwePersonenAuto.setGewicht(scanner.nextInt());
+        System.out.print("Motorinhoud in cc: ");
+        nieuwePersonenAuto.setMotorInhoud(scanner.nextInt());
+        productInventaris.addProduct(nieuwePersonenAuto);
+    }
+    void voegVrachtautoToe(){
+        System.out.println("Vrachtwagen");
+        VrachtAuto nieuweVrachtAuto = ProductFactory.Vrachtauto.createProduct();
+        System.out.print("Omschrijving: ");
+        nieuweVrachtAuto.setOmschrijving(scanner.nextLine());
+        System.out.print("Laadvermogen in kg: ");
+        nieuweVrachtAuto.setLaadVermogen(scanner.nextInt());
+        System.out.print("Motorinhoud in cc: ");
+        nieuweVrachtAuto.setMotorInhoud(scanner.nextInt());
+        productInventaris.addProduct(nieuweVrachtAuto);
+    }
     @Override
     public void sluitVenster() {
         Venster Beheer = new Beheer(getMedewerker(), productInventaris);
